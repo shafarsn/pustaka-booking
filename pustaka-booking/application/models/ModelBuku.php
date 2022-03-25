55489cc56b4e44a8984cbbbc2028b78a4c0090ae
shafarsn <rsnshafa19@gmail.com>
2022-03-25 22:34:00 +0700

    katalog buku

--- pustaka-booking/application/models/ModelBuku.php
+++ pustaka-booking/application/models/ModelBuku.php
@@ -54,13 +54,13 @@ class ModelBuku extends CI_Model
  {
  $this->db->update('kategori', $data, $where);
  }
- //join
+//join
  public function joinKategoriBuku($where)
  {
- $this->db->select('buku.id_kategori,kategori.kategori');
+ //$this->db->select('buku.id_kategori,kategori.kategori');
+ $this->db->select('*');
  $this->db->from('buku');
- $this->db->join('kategori','kategori.id = 
-buku.id_kategori');
+ $this->db->join('kategori','kategori.id =  buku.id_kategori');
  $this->db->where($where);
  return $this->db->get();
  }
