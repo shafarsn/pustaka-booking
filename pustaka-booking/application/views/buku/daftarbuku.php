53539bcb943f27c5d5ccbcfb392e735f04de8cbf
shafarsn <rsnshafa19@gmail.com>
2022-03-26 21:37:37 +0700

    katalog buku

--- pustaka-booking/application/views/buku/daftarbuku.php
+++ pustaka-booking/application/views/buku/daftarbuku.php
@@ -6,7 +6,7 @@
  <!-- Tampilkan semua produk -->
  <div class="row">
  <!-- looping products -->
- <?php foreach ($buku as $buku)  ?>
+ <?php foreach ($buku as $buku) { ?>
  <div class="col-md-2 col-md-3">
  <div class="thumbnail" style="height: 370px;">
  <img src="<?php echo base_url(); ?>assets/img
@@ -16,7 +16,7 @@
  <h5><?= $buku->penerbit ?></h5>
  <h5><?= substr($buku->tahun_terbit, 0, 4) ?></h5>
 <?= $this->session->flashdata('pesan'); ?>
- 
+
  <div style="padding: 25px;">
  <div class="x_panel">
  <div class="x_content">
